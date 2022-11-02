65f00238cd740d5e1b1b0c1ee3054a4f34fb77b0
PenguinsSiena <105580668+PenguinsSiena@users.noreply.github.com>
2022-11-02 22:45:36 +1100

    Add files via upload

---
+++ co1.js
@@ -0,0 +1,6 @@
+function btn2(){
+    window.location = "index 2.html";
+}
+function btn3(){
+    window.location = "index.html";
+}
---
+++ co2.js
@@ -0,0 +1,6 @@
+function btn3(){
+    window.location = "index.html";
+}
+function btn1(){
+    window.location = "index copy 2.html";
+}
